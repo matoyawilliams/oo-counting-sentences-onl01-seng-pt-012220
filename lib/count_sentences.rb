ae0e48bb2a6a02241866eccec1d577c5f2b8bdea
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-20 03:48:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,8 +17,8 @@ class String
   end
   
   def count_sentences_words
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    # splits string into Array, collects those would-be sentences that actually contain word characters, removes false values, then counts the remaining booleans.
+  end
+
 end
-
-end 
-
